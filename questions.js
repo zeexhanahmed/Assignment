f6acb8fc5acd896f6db60617b519c80d6bd49440
zeexhanahmed <zeeshanahmed.official02@gmail.com>
2023-11-05 21:16:38 +0500

    second page done

---
+++ questions.js
@@ -0,0 +1,5 @@
+
+var questions = [
+    "Input two characters and print them on the next line in the reverse order.",
+    "Input two characters and swap them without using third variable and print them on the next line."
+];
