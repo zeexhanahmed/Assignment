87a19d7138185d2b4a66bfc3b6abbf131a14b5fc
zeexhanahmed <zeeshanahmed.official02@gmail.com>
2024-01-07 11:58:31 +0500

    modified

--- questions.js
+++ questions.js
@@ -1,337 +1,315 @@
 
-var questions = [
-    "Input two characters and print them on the next line in the reverse order.",
-    "Input two characters and swap them without using third variable and print them on the next line.",
-    "To find out volume of sphere and area of triangle, given radius as input",
-    "To check whether two inputted numbers are same or not",
-    "To check whether an input no. is possitive or negative",
-    "To check whether an input year is leap year or not",
-    "To check whether the input character is uppercase or lowercase",
-    "To check whether the input alphabet is vowel or not",
-    "To convert an inputted alphabet from upper to lower case and from lower to upper case",
-    "To check whether an input no. is even or odd",
-    "To input marks of 5 subject, then calculate and print percentage and grade",
-    "Take 5 digit no. and print every digit on the next line",
-    "To display all numbers from 1 to N",
-    "To display the sum of all numbers from 1 to N",
-    "To display the product of all numbers from 1 to N",
-    "To display the factorial of given integer",
-    "To display the squares of all number from 1 to N",
-    "Take a number as input and print all of its divisors",
-    "To print the no. from 1 to N along with indication whether each is even or odd ",
-    "To print the power of A power B using user defined function where A and B are integer input",
-    "To display all number from 1 to N and then folloed by N-1 to 1",
-    "To calculate the sum of input integer until user input negative no",
-    "Take a number and display it in reverse order",
-    "Take a five digit number and print the reverse on the next line",
-    "To read and display integer array of 10 elements",
-    "Take input from user and count and then print total no. of word, vowel and character",
-    "To calculate the sum of all number from 1 to N, at the end user will be asked whether to continue or not",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "To display the following pattern",
-    "Write a program to input a number N, and then generate the following pattern",
-    "To display the sum of all element in an integer array",
-    "To print the maximum and minimum number from an integer array of 10 element",
-    "To display all element of an integer array with an indication whether each is even or odd also display the count of even and odd number",
-    "To search a given number in an integer array of 10 element (Linear Search)",
-    "To sort given integer array of 10 element in ascending order",
-    "To sort given integer array of 10 element in decending order",
-    "To spell the inputted integer in words",
-    "To calculate and print the average of all element in a given integer array",
-    "To display the given integer array of 10 element such as the first element is displayed once, second is displayed twice and so on",
-    "To check whether input string is palindrome or not",
-    "To input a string and display it without displaying vowels",
-    "To display the reverse of a string without using strrev() function",
-    "To input a multi word string and then display each word on next line",
-    "To reverse each word of an inputted multi word string",
-    "To convert all uppercase into lowercsae and lowercsae into uppercase without using library function",
-    "That count the frequency of character in an inputted string",
-    "Read 10 elements in an integer array with a check that no number is entered again",
-    "Read 10 element in an integer array, then display only those element that are divisible by K (where K is an input number)",
-    "Take two integer array and combine them in such a way that no element is repeated again",
-    "That take two integer array and print the intersections of two arrays"
-];
-
-
 var answer = [
     {
+        "question" : "Input two characters and print them on the next line in the reverse order.",
         "answer" : "Example",
         "link" : "Assignment-1.html",
         "codeLink" : "code/AssignmentCode-1.html"
     },
 
     {
+        "question" : "Input two characters and swap them without using third variable and print them on the next line.",
         "answer" : "Example",
         "link" : "Assignment-2.html",
         "codeLink" : "code/AssignmentCode-2.html" 
     },
 
     {
+        "question" : "To find out volume of sphere and area of triangle, given radius as input",
         "answer" : "Example",
         "link" : "Assignment-3.html",
         "codeLink" : "code/AssignmentCode-3.html"
     },
 
     {
+        "question" : "To check whether two inputted numbers are same or not",
         "answer" : "Example",
         "link" : "Assignment-4.html",
         "codeLink" : "code/AssignmentCode-4.html"
     },
 
     {
+        "question" : "To check whether an input no. is possitive or negative",
         "answer" : "Example",
         "link" : "Assignment-5.html",
         "codeLink" : "code/AssignmentCode-5.html"
     },
 
     {
+        "question" : "To check whether an input year is leap year or not",
         "answer" : "Example",
         "link" : "Assignment-6.html",
         "codeLink" : "code/AssignmentCode-6.html"
     },
 
     {
+        "question" : "To check whether the input character is uppercase or lowercase",
         "answer" : "Example",
         "link" : "Assignment-7.html",
         "codeLink" : "code/AssignmentCode-7.html"
     },
 
     {
+        "question" : "To check whether the input alphabet is vowel or not",
         "answer" : "Example",
         "link" : "Assignment-8.html",
         "codeLink" : "code/AssignmentCode-8.html"
     },
 
     {
+        "question" : "To convert an inputted alphabet from upper to lower case and from lower to upper case",
         "answer" : "Example",
         "link" : "Assignment-9.html",
         "codeLink" : "code/AssignmentCode-9.html"
     },
 
     {
+        "question" : "To check whether an input no. is even or odd",
         "answer" : "Example",
         "link" : "Assignment-10.html",
         "codeLink" : "code/AssignmentCode-10.html"
     },
 
     {
+        "question" : "To input marks of 5 subject, then calculate and print percentage and grade",
         "answer" : "Example",
         "link" : "Assignment-11.html",
         "codeLink" : "code/AssignmentCode-11.html"
     },
 
     {
+        "question" : "Take 5 digit no. and print every digit on the next line",
         "answer" : "Example",
         "link" : "Assignment-12.html",
         "codeLink" : "code/AssignmentCode-12.html"
     },
 
     {
+        "question" : "To display all numbers from 1 to N",
         "answer" : "Example",
         "link" : "Assignment-13.html",
         "codeLink" : "code/AssignmentCode-13.html"
     },
 
     {
+        "question" : "To display the sum of all numbers from 1 to N",
         "answer" : "Example",
         "link" : "Assignment-14.html",
         "codeLink" : "code/AssignmentCode-14.html"
     },
 
     {
+        "question" : "To display the product of all numbers from 1 to N",
         "answer" : "Example",
         "link" : "Assignment-15.html",
         "codeLink" : "code/AssignmentCode-15.html"
     },
 
     {
+        "question" :  "To display the factorial of given integer",
         "answer" : "Example",
         "link" : "Assignment-16.html",
         "codeLink" : "code/AssignmentCode-16.html"
     },
 
     {
+        "question" : "To display the squares of all number from 1 to N",
         "answer" : "Example",
         "link" : "Assignment-17.html",
         "codeLink" : "code/AssignmentCode-17.html"
     },
 
     {
+        "question" : "Take a number as input and print all of its divisors",
         "answer" : "Example",
         "link" : "Assignment-18.html",
         "codeLink" : "code/AssignmentCode-18.html"
     },
 
     {
+        "question" : "To print the no. from 1 to N along with indication whether each is even or odd ",
         "answer" : "Example",
         "link" : "Assignment-19.html",
         "codeLink" : "code/AssignmentCode-19.html"
     },
 
     {
+        "question" :  "To print the power of A power B using user defined function where A and B are integer input",
         "answer" : "Example",
         "link" : "Assignment-20.html",
         "codeLink" : "code/AssignmentCode-20.html"
     },
 
     {
+        "question" : "To display all number from 1 to N and then folloed by N-1 to 1",
         "answer" : "Example",
         "link" : "Assignment-21.html",
         "codeLink" : "code/AssignmentCode-21.html"
     },
 
     {
+        "question" : "To calculate the sum of input integer until user input negative no",
         "answer" : "Example",
         "link" : "Assignment-22.html",
         "codeLink" : "code/AssignmentCode-22.html"
     },
 
     {
+        "question" : "Take a number and display it in reverse order",
         "answer" : "Example",
         "link" : "Assignment-23.html",
         "codeLink" : "code/AssignmentCode-23.html"
     },
 
     {
+        "question" : "Take a five digit number and print the reverse on the next line",
         "answer" : "Example",
         "link" : "Assignment-24.html",
         "codeLink" : "code/AssignmentCode-24.html"
     },
 
     {
+        "question" : "To read and display integer array of 10 elements",
         "answer" : "Example",
         "link" : "Assignment-25.html",
         "codeLink" : "code/AssignmentCode-25.html"
     },
 
     {
+        "question" : "Take input from user and count and then print total no. of word, vowel and character",
         "answer" : "Example",
         "link" : "Assignment-26.html",
         "codeLink" : "code/AssignmentCode-26.html"
     },
 
     {
+        "question" : "To calculate the sum of all number from 1 to N, at the end user will be asked whether to continue or not",
         "answer" : "Example",
         "link" : "Assignment-27.html",
         "codeLink" : "code/AssignmentCode-27.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-28.html",
         "codeLink" : "code/AssignmentCode-28.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-29.html",
         "codeLink" : "code/AssignmentCode-29.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-30.html",
         "codeLink" : "code/AssignmentCode-30.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-31.html",
         "codeLink" : "code/AssignmentCode-31.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-32.html",
         "codeLink" : "code/AssignmentCode-32.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-33.html",
         "codeLink" : "code/AssignmentCode-33.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-34.html",
         "codeLink" : "code/AssignmentCode-34.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-35.html",
         "codeLink" : "code/AssignmentCode-35.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-36.html",
         "codeLink" : "code/AssignmentCode-36.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-37.html",
         "codeLink" : "code/AssignmentCode-37.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-38.html",
         "codeLink" : "code/AssignmentCode-38.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-39.html",
         "codeLink" : "code/AssignmentCode-39.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-40.html",
         "codeLink" : "code/AssignmentCode-40.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-41.html",
         "codeLink" : "code/AssignmentCode-41.html"
     },
 
     {
+        "question" : "To display the following pattern",
         "answer" : "Example",
         "link" : "Assignment-42.html",
         "codeLink" : "code/AssignmentCode-42.html"
     },
 
     {
+        "question" : "Write a program to input a number N, and then generate the following pattern",
         "answer" : "Example",
         "link" : "Assignment-43.html",
         "codeLink" : "code/AssignmentCode-43.html"
     },
 
     {
+        "question" : "To display the sum of all element in an integer array",
         "answer" : "Example",
         "link" : "Assignment-44.html",
         "codeLink" : "code/AssignmentCode-44.html"
     },
 
     {
+        "question" : "To print the maximum and minimum number from an integer array of 10 element",
         "answer" : "Example",
         "link" : "Assignment-45.html",
         "codeLink" : "code/AssignmentCode-45.html"
@@ -339,6 +317,7 @@ var answer = [
 
 
     {
+        "question" :  "To display all element of an integer array with an indication whether each is even or odd also display the count of even and odd number",
         "answer" : "Example",
         "link" : "Assignment-46.html",
         "codeLink" : "code/AssignmentCode-46.html"
@@ -346,6 +325,7 @@ var answer = [
 
 
     {
+        "question" : "To search a given number in an integer array of 10 element (Linear Search)",
         "answer" : "Example",
         "link" : "Assignment-47.html",
         "codeLink" : "code/AssignmentCode-47.html"
@@ -353,6 +333,7 @@ var answer = [
 
 
     {
+        "question" : "To sort given integer array of 10 element in ascending order",
         "answer" : "Example",
         "link" : "Assignment-48.html",
         "codeLink" : "code/AssignmentCode-48.html"
@@ -360,90 +341,105 @@ var answer = [
 
 
     {
+        "question" : "To sort given integer array of 10 element in decending order",
         "answer" : "Example",
         "link" : "Assignment-49.html",
         "codeLink" : "code/AssignmentCode-49.html"
     },
 
     {
+        "question" : "To spell the inputted integer in words",
         "answer" : "Example",
         "link" : "Assignment-50.html",
         "codeLink" : "code/AssignmentCode-50.html"
     },
 
     {
+        "question" : "To calculate and print the average of all element in a given integer array",
         "answer" : "Example",
         "link" : "Assignment-51.html",
         "codeLink" : "code/AssignmentCode-51.html"
     },
 
     {
+        "question" : "To display the given integer array of 10 element such as the first element is displayed once, second is displayed twice and so on",
         "answer" : "Example",
         "link" : "Assignment-52.html",
         "codeLink" : "code/AssignmentCode-52.html"
     },
 
     {
+        "question" : "To check whether input string is palindrome or not",
         "answer" : "Example",
         "link" : "Assignment-53.html",
         "codeLink" : "code/AssignmentCode-53.html"
     },
 
     {
+        "question" : "To input a string and display it without displaying vowels",
         "answer" : "Example",
         "link" : "Assignment-54.html",
         "codeLink" : "code/AssignmentCode-54.html"
     },
 
     {
+        "question" : "To display the reverse of a string without using strrev() function",
         "answer" : "Example",
         "link" : "Assignment-55.html",
         "codeLink" : "code/AssignmentCode-55.html"
     },
 
     {
+        "question" : "To input a multi word string and then display each word on next line",
         "answer" : "Example",
         "link" : "Assignment-56.html",
         "codeLink" : "code/AssignmentCode-56.html"
     },
 
     {
+        "question" : "To reverse each word of an inputted multi word string",
         "answer" : "Example",
         "link" : "Assignment-57.html",
         "codeLink" : "code/AssignmentCode-57.html"
     },
 
     {
+        "question" : "To convert all uppercase into lowercsae and lowercsae into uppercase without using library function",
         "answer" : "Example",
         "link" : "Assignment-58.html",
         "codeLink" : "code/AssignmentCode-58.html"
     },
 
     {
+        "question" : "That count the frequency of character in an inputted string",
         "answer" : "Example",
         "link" : "Assignment-59.html",
         "codeLink" : "code/AssignmentCode-59.html"
     },
 
     {
+        "question" : "Read 10 elements in an integer array with a check that no number is entered again",
         "answer" : "Example",
         "link" : "Assignment-60.html",
         "codeLink" : "code/AssignmentCode-60.html"
     },
 
     {
+        "question" : "Read 10 element in an integer array, then display only those element that are divisible by K (where K is an input number)",
         "answer" : "Example",
         "link" : "Assignment-61.html",
         "codeLink" : "code/AssignmentCode-61.html"
     },
 
     {
+        "question" : "Take two integer array and combine them in such a way that no element is repeated again",
         "answer" : "Example",
         "link" : "Assignment-62.html",
         "codeLink" : "code/AssignmentCode-62.html"
     },
 
     {
+        "question" : "That take two integer array and print the intersections of two arrays",
         "answer" : "Example",
         "link" : "Assignment-63.html",
         "codeLink" : "code/AssignmentCode-63.html"
